6c0fdb4d8c64139b4b03c5d0b5c8f8e27e9f2073
bernardpons <bpons@noos.fr>
2019-09-29 19:03:55 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "zones" {
+    type    = "list"
+    default = ["europe-west1-b", "europe-west1-c", "europe-west1-d"]
+}
+
+variable "region" {
+    type    = "string"
+    default = "europe-west1"
+}
