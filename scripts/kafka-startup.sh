167974c7d1504955fd838d78db461bafec242092
bernardpons <bpons@noos.fr>
2019-10-01 18:04:51 +0200

    change fstab

--- scripts/kafka-startup.sh
+++ scripts/kafka-startup.sh
@@ -3,7 +3,10 @@ sudo su
 
 echo "10.1.1.11 zoo1
 10.1.1.12 zoo2
-10.1.1.13 zoo3" | tee --append /etc/hosts
+10.1.1.13 zoo3
+10.1.1.21 kafka1
+10.1.1.22 kafka2
+10.1.1.23 kafka3" | tee --append /etc/hosts
 
 echo "* hard nofile 100000
 * soft nofile 100000" | tee --append /etc/security/limits.conf
@@ -11,12 +14,12 @@ echo "* hard nofile 100000
 echo 'type=83' | sudo sfdisk /dev/sdb
 mkfs.xfs -f /dev/sdb
 mkdir -p /data/kafka
-mount -t xfs /dev/sdb /data/kafka
+echo '/dev/sdb /data/kafka xfs user,defaults 0 0' >> /etc/fstab
+mount -a
 chown -R kafka:kafka /data/kafka
-echo '/dev/sdb /data/kafka xfs defaults 0 0' >> /etc/fstab
 
 sed -i 's/{{broker_id}}/${broker_id}/g' /opt/kafka/config/server.properties
 
-sudo systemctl enable kafka
-sudo systemctl start kafka
+systemctl enable kafka
+systemctl start kafka
 
